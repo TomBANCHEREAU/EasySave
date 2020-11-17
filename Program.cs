4a354091c5f389b3910fd9deb2ae33cf415190e2
TomBANCHEREAU <banchereau.tom@gmail.com>
2020-11-17 14:04:41 +0100

    chore: add VS project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EasySave.Views;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            // new ConsoleMenu().Start();
+        }
+    }
+}
