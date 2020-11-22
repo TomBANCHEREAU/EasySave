31d24f3e87d984da75092b0c9c1de6a8b74e2708
TomBANCHEREAU <banchereau.tom@gmail.com>
2020-11-22 12:21:20 +0100

    feat: add logic for running and restoring full backup

--- Models/FullBackupStrategy.cs
+++ Models/FullBackupStrategy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EasySave.Models
@@ -8,12 +9,29 @@ namespace EasySave.Models
     {
         public void Execute(Backup backup)
         {
-            throw new NotImplementedException();
+            copyContent(backup.BackupEnvironment.SourceDirectory, backup.BackupEnvironment.DestinationDirectory);
         }
 
         public void Restore(Backup backup)
         {
-            throw new NotImplementedException();
+            copyContent(backup.BackupEnvironment.DestinationDirectory, backup.BackupEnvironment.SourceDirectory);
+        }
+        private void copyContent(String srcBasePath, String destBasePath,String pathFromBase = "")
+        {
+            String srcPath = Path.Join(srcBasePath,pathFromBase);
+            String destPath = Path.Join(destBasePath, pathFromBase);
+            
+            foreach (String filePath in Directory.EnumerateFiles(srcPath))
+            {
+                String fileName = Path.GetFileName(filePath);
+                File.Copy(Path.Join(srcPath, fileName), Path.Join(destPath, fileName),true);
+            }
+            foreach (String dirPath in Directory.EnumerateDirectories(srcPath))
+            {
+                String dirName = Path.GetFileName(dirPath);
+                Directory.CreateDirectory(Path.Join(destPath, dirName));
+                copyContent(srcBasePath, destBasePath, Path.Join(pathFromBase, dirName));
+            }
         }
     }
 }
