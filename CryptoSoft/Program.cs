0439ebb354746f5b2771ddcbbe705ec2cb59ac3d
TomBANCHEREAU <banchereau.tom@gmail.com>
2020-12-01 17:54:02 +0100

    chore: add project for CryptoSoft

---
+++ CryptoSoft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
