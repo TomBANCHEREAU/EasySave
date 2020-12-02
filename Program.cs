85827ad66bd322df6da62322cf8589c7cb20f5c2
TomBANCHEREAU <banchereau.tom@gmail.com>
2020-12-02 14:21:35 +0100

    chore: change app type to windows app

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
+using System.Windows.Forms;
 using EasySave.Controllers;
 using EasySave.Models;
 using EasySave.Views;
 using EasySave.Views.ConsoleViews;
+using EasySave.Views.WindowsFormViews;
 
 namespace EasySave
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             IModel model = new Model();
             IView view = new ConsoleView();
             IController controller = new Controller(model,view);
             controller.Start();
+            /*Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainView());*/
         }
     }
 }
