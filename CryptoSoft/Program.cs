c81336cb5e0744b4a304119d64f9d0538e40cfa6
Maxence BONNEAU <bonneau.maxence@hotmail.fr>
2020-12-03 09:33:58 +0100

    feat: add Cryptosoft

--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
 
 namespace CryptoSoft
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string OutputFile = @"C:\Users\bonne\OneDrive\Documents\cours exia\Programmation système\EasySave\OutEncryption.txt";
+            byte[] data = File.ReadAllBytes(@"C:\Users\bonne\OneDrive\Documents\cours exia\Programmation système\EasySave\point d'armée.txt");
+
+            byte[] key = File.ReadAllBytes(@"C:\Users\bonne\OneDrive\Documents\cours exia\Programmation système\EasySave\key.txt" );
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                byte x = key[(byte)(i % key.Length)];
+                data[i] = (byte)(data[i] ^ x);
+            }
+            File.WriteAllBytes(OutputFile, data);
+
+            return 4; 
+
         }
     }
 }
